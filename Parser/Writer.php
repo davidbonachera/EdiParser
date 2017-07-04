97980de7fff6d02b3994b4ca7d0bca0ae2b562cd
David Bonachera <david.bonachera@gmail.com>
2017-07-04 15:36:01 +0800

    Write positional lines

--- Parser/Writer.php
+++ Parser/Writer.php
@@ -13,33 +13,81 @@ use Boda\EdiParserBundle\Model\ModelAbstract;
 class Writer
 {
     /**
-     * @var array
-     */
-    private $errors = [];
-
-    /**
      * Model
      *
      * @var ModelAbstract
      */
     private $model;
 
-    public function __construct()
+    public function __construct(ModelAbstract $model)
     {
-        // TODO: Implement parse() method.
+        $this->setModel($model);
     }
 
-    public function write()
+    public function writeFile($path)
     {
+        $file = fopen($path."M20-".date("YmdHis").".txt", "w") or die("Unable to open file!");
+        // Write Header
+        $str = $this->writeLine($this->model->getHeader(), $this->model->getValidationTemplateHeader())."\n";
+        fwrite($file, $str);
+        $data = $this->model->getData();
+        // Write Data
+        foreach ($data as $lineKey => $lineData) {
+            // ToDo : Fix and find a way to identify the header in a proper way (cf end of line)
+            $str = $this->writeLine($lineData, $this->model->getValidationTemplateData()[$lineData["CODEXC"].$lineData["SEPEXC"].$lineData["SCOEXC"]])."\n";
+            fwrite($file, $str);
+        }
+        // Write Footer
+        $str = $this->writeLine($this->model->getFooter(), $this->model->getValidationTemplateFooter())."\n";
+        fwrite($file, $str);
+        fclose($file);
         return true;
+    }
+
+    public function writeLine($line, $validationTemplate){
+        $str = "";
+        $pos = 0;
+        $i=0;
+        foreach ($line as $key => $value) {
+            $i++;
+            if(is_null($value)) {
+                $value = str_repeat(" ", $validationTemplate[$key]["length"]);
+            } else {
+                if(!empty($validationTemplate[$key]["numerical"])) {
+                    $value = str_repeat("0", $validationTemplate[$key]["length"]-strlen($value)).$value;
+                } else {
+                    $value = $value.str_repeat(" ", $validationTemplate[$key]["length"]-strlen($value));
+                }
+
+            }
+            $str = substr_replace($str, $value, $pos, 0);
+            $pos += $validationTemplate[$key]["length"];
+        };
+        return $str;
+    }
+
+    public function getTemplateHeader(){
+        return $this->model->getTemplateHeader();
+    }
+
+    public function getTemplateData(){
+        return $this->model->getTemplateData();
+    }
+
+    public function getTemplateFooter(){
+        return $this->model->getTemplateFooter();
     }
 
     public function setHeader(array $data) {
         $this->model->setHeader($data);
     }
 
-    public function insertOne(array $data) {
-        $this->model;
+    public function insertOne(array $data, $dataKey) {
+        $this->model->insertOne($data, $dataKey);
+    }
+
+    public function setFooter(array $data) {
+        $this->model->setFooter($data);
     }
 
     /**
@@ -56,13 +104,5 @@ class Writer
     public function setModel($model)
     {
         $this->model = $model;
-    }
-
-    /**
-     * Get errors
-     */
-    public function errors()
-    {
-        return $this->errors;
     }
 }
