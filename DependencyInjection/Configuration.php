33a183ff6ca2b1b308b624f6a9fbba105bbda185
David Bonachera <david.bonachera@gmail.com>
2019-12-10 14:34:06 +0800

    Develop (#6)
    * ✨ M30 Corrected + Default behavior for header
    
    * ✨Added M30.01 segment
    
    * ␡ Deleted some default var
    
    Those variables need to be set dynamically.
    
    * 🤦‍♂️ Oops.
    
    .....
    
    * Update Writer.php
    
    Infolog standard, EVERYTHING CAPS !
    
    * Fix symfony 4.2 deprecations
    
    TreeBuilder need the rootNode specified in the TreeBuilder class.
    
    * Update Writer.php
    
    Linting + cache writing.
    
    * Update Writer.php
    
    :-1:

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('boda_edi_parser');
+        $treeBuilder = new TreeBuilder('boda_edi_parser');
+        $rootNode = $treeBuilder->getRootNode();
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
